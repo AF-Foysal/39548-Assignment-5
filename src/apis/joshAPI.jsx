3f04c59cc705d7e16f94c8dc92c3d07d3d018f6a
JoshuaRamc <joshuaramcharan818@gmail.com>
2023-11-21 20:47:34 -0500

    Add code to use currency state from redux store

--- src/apis/joshAPI.jsx
+++ src/apis/joshAPI.jsx
@@ -1,29 +1,45 @@
 import { useState } from "react";
+import { changeCurrency } from "../cashSlice";
+import { useSelector, useDispatch } from "react-redux";
+
 export default function JoshAPI() {
   const url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+  const tempCurr = useSelector((state) => state.cash.curr);
   const [currency, setCurrency] = useState("USD");
+  const dispatch = useDispatch();
   const [input, setInput] = useState("USD");
 
-  function changeCurrency(e) {
-    setInput(e.target.value);
+  function changeCurr(e) {
+    const tempInput = e.target.value;
+    setInput(tempInput);
   }
 
   async function getCurrencyData() {
-    if (input === "USD" || input === "EUR" || input === "GBP") {
+    console.log("redux state", tempCurr);
+    console.log("input", input);
+    if (tempCurr === "USD" || tempCurr === "EUR" || tempCurr === "GBP") {
       fetch(url)
         .then((response) => response.json())
         .then((data) => {
-          setCurrency(data.bpi[input]);
+          setCurrency(data.bpi[tempCurr]);
           console.log(data);
         })
         .catch((error) => console.error(error));
     }
   }
+  const newTemp = input;
+  dispatch(changeCurrency(newTemp));
 
   return (
     <>
-      <input onChange={changeCurrency} value={input} />
-      <button onClick={getCurrencyData}>Find Bitcoin Value</button>
+      <input onChange={changeCurr} value={input} />
+      <button
+        onClick={() => {
+          getCurrencyData();
+        }}
+      >
+        Find Bitcoin Value
+      </button>
       <h1 style={{ color: "purple", fontSize: "2rem" }}>
         Enter currencies USD, EUR, or GBP
       </h1>
